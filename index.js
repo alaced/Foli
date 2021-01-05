a16f4c1ea57ccd7943308751788a40bc51aa769e
alaced <58386621+alaced@users.noreply.github.com>
2021-01-06 01:48:51 +0900

    add page

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-const exploreBtn = document.querySelector(".js-exploreBtn"),
-  motiveTab = document.querySelector(".js-motive"),
-  closeBtn = document.querySelector(".js-close"),
-  carousel = document.querySelectorAll(".carousel"),
-  prodList = document.querySelectorAll(".prod__list"),
-  prodItem = document.querySelectorAll(".prod__item"),
-  prodDesc = document.querySelectorAll(".prod__desc"),
+const carousel = document.querySelectorAll(".carousel"),
+  contList = document.querySelectorAll(".content__list"),
+  contItem = document.querySelectorAll(".content__item"),
+  contDesc = document.querySelectorAll(".content__desc"),
   indicator = document.querySelector(".carousel-indicators"),
-  prodImg01 = document.querySelector(".prod__img01"),
-  prodImg02 = document.querySelector(".prod__img02"),
-  prodImg03 = document.querySelector(".prod__img03"),
-  prodPrice = document.querySelectorAll(".prod__price"),
+  contImg01 = document.querySelector(".content__img01"),
+  contImg02 = document.querySelector(".content__img02"),
+  contImg03 = document.querySelector(".content__img03"),
   more01 = document.querySelector(".more01"),
   more02 = document.querySelector(".more02"),
   more03 = document.querySelector(".more03");
-
-exploreBtn.addEventListener("click", function (e) {
-  motiveTab.classList.remove("none");
-  motiveTab.classList.add("flex");
-});
-closeBtn.addEventListener("click", function (e) {
-  motiveTab.classList.add("none");
-  motiveTab.classList.remove("flex");
-});
-
-more01.addEventListener("mouseenter", function (e) {
-  prodImg01.setAttribute("src", "images/doc02.png");
-});
 
 let carouselSize = window.matchMedia("(max-width: 700px)");
 function carouselHandler() {
@@ -33,13 +16,13 @@ function carouselHandler() {
     carousel.forEach(function (caro) {
       caro.classList.add("slide");
     });
-    prodList.forEach(function (list) {
+    contList.forEach(function (list) {
       list.classList.add("carousel-inner");
     });
-    prodItem.forEach(function (item) {
+    contItem.forEach(function (item) {
       item.classList.add("carousel-item");
     });
-    prodDesc.forEach(function (desc) {
+    contDesc.forEach(function (desc) {
       desc.classList.add("carousel-caption");
     });
     indicator.classList.remove("none");
@@ -47,13 +30,13 @@ function carouselHandler() {
     carousel.forEach(function (caro) {
       caro.classList.remove("slide");
     });
-    prodList.forEach(function (list) {
+    contList.forEach(function (list) {
       list.classList.remove("carousel-inner");
     });
-    prodItem.forEach(function (item) {
+    contItem.forEach(function (item) {
       item.classList.remove("carousel-item");
     });
-    prodDesc.forEach(function (desc) {
+    contDesc.forEach(function (desc) {
       desc.classList.remove("carousel-caption");
     });
     indicator.classList.add("none");
@@ -64,20 +47,20 @@ carouselHandler();
 carouselSize.addEventListener("change", carouselHandler);
 
 more01.addEventListener("mouseenter", function (e) {
-  prodImg01.setAttribute("src", "images/doc02.png");
+  contImg01.setAttribute("src", "images/doc02.png");
 });
 more01.addEventListener("mouseleave", function (e) {
-  prodImg01.setAttribute("src", "images/doc01.png");
+  contImg01.setAttribute("src", "images/doc01.png");
 });
 more02.addEventListener("mouseenter", function (e) {
-  prodImg02.setAttribute("src", "images/manual02.png");
+  contImg02.setAttribute("src", "images/manual02.png");
 });
 more02.addEventListener("mouseleave", function (e) {
-  prodImg02.setAttribute("src", "images/manual01.png");
+  contImg02.setAttribute("src", "images/manual01.png");
 });
 more03.addEventListener("mouseenter", function (e) {
-  prodImg03.setAttribute("src", "images/test02.png");
+  contImg03.setAttribute("src", "images/test02.png");
 });
 more03.addEventListener("mouseleave", function (e) {
-  prodImg03.setAttribute("src", "images/test01.png");
+  contImg03.setAttribute("src", "images/test01.png");
 });
